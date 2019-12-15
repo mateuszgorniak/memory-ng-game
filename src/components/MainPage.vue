d868fee223f57db03166a92c4678698cb124a367
Mateusz Górniak <mateusz.gorniak@netguru.pl>
2019-12-15 15:39:13 +0100

    Add basic structure of the main page and two templates for basic components

---
+++ src/components/MainPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <scoreboard></scoreboard>
+    <board></board>
+  </div>
+</template>
+
+<script>
+import Board from './Board';
+import Scoreboard from './Scoreboard';
+
+export default {
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
