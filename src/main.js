686ec17bd81b2e40a59bdee67fcb0b189c632b60
Mateusz Górniak <mateusz.gorniak@netguru.pl>
2019-12-16 00:24:49 +0100

    Install Vuex to handle app-wide store

--- src/main.js
+++ src/main.js
@@ -1,14 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import Vuex from 'vuex';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+Vue.use(Vuex);
 
 /* eslint-disable no-new */
+const store = new Vuex.Store({
+  state: {
+    cards: [],
+    moves: 0,
+  },
+});
+
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>',
